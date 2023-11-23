56359ecf78da64a7393e9cf2e3c267ecaef01963
MRyabchikov <m.ryabchikov@yandex.ru>
2023-11-24 01:56:43 +0300

    Change colors

--- cell.cpp
+++ cell.cpp
@@ -9,30 +9,35 @@ Cell::Cell(Point xy, Callback cb, Type t) : Button{xy, size, size, "", cb}, type
 
 void Cell::reset_color()
 {
-    if (!pw) error("Cell is not attached to a window");
+    if (!pw)
+        error("Cell is not attached to a window");
     if (is_black())
-        pw->color(Color::chess_green);
+        pw->color(Color::black);
     else
-        pw->color(Color::chess_white);
+        pw->color(Color::white);
 }
 
-void Cell::attach(Graph_lib::Window& win) {
+void Cell::attach(Graph_lib::Window& win)
+{
     Button::attach(win);
     reset_color();
 }
 
-Form& Cell::detach_form() {
-    Form * ch = form;
+Form& Cell::detach_form()
+{
+    Form* ch = form;
     form = nullptr;
-    ch -> detach();
+    ch->detach();
     return *ch;
 }
 
-void Cell::attach_form(Form& ch) {
+void Cell::attach_form(Form& ch)
+{
     ch.attach(*this);
     form = &ch;
 }
 
-const Form& Cell::get_form() const { // обязательно нужна проверка не нулевой ли указатель checker
+const Form& Cell::get_form() const
+{  // обязательно нужна проверка не нулевой ли указатель checker
     return *form;
 }
